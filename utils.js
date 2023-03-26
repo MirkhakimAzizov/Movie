59f1f01c1b60572440a3ded7e8cb2d6513ad3254
MirkhakimAzizov <azizov.mirkhakimjon@gmail.com>
2023-03-26 09:05:48 +0500

    movies

--- utils.js
+++ utils.js
@@ -13,11 +13,20 @@ function renderUi(arr) {
                 <h5 class="card-title">${item.title}</h5>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">Language: ${item.lang}</li>
-                <li class="list-group-item mr-4"> Rating: ${item.rating}</li>
-                <li class="list-group-item">${item.categories}</li>
                 <li class="list-group-item">
-                    <a class="card-link btn btn-success w-100 mx-auto mb-2 btn__more" data-more="${item.id}" >More</a>
+                    <div class="d-flex gap-4">
+                        <span class="text-warning d-flex align-items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            Rating: ${item.rating}</span>
+                        <span class="text-primary  text-decoration-underline">${item.time}</span> </div>
+                    <div>Language: ${item.lang}</div>
+                    <div>Year: ${item.year}</div>
+                    <div></div>
+                </li>
+                <li class="list-group-item">
+                    <a class="card-link btn btn-success w-100 mx-auto mb-2 btn__more" data-more="${item.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">More</a>
                     <a href="${item.link}" class="card-link btn btn-danger w-100 mx-auto mb-2">YouTube</a>
                     <a class="card-link btn btn-info btn__add w-100 mx-auto" data-id="${item.id}">ADD
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
@@ -49,10 +58,9 @@ function renderBookmark(arr) {
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">Language: ${item.lang}</li>
-                <li class="list-group-item mr-4"> Rating: ${item.rating}</li>
                 <li class="list-group-item">${item.categories}</li>
                 <li class="list-group-item">
-                    <a class="card-link btn btn-success w-100 mx-auto mb-2" data-more="${item.id}" >More</a>
+                    <a class="card-link btn btn-success w-100 mx-auto mb-2" data-more="${item.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">More</a>
                     <a href="${item.link}" class="card-link btn btn-danger w-100 mx-auto mb-2">YouTube</a>
                     <a class="card-link btn btn-warning btn__add w-100 mx-auto" data-id="${item.id}">REMOVE</a>
                 </li>
@@ -69,16 +77,17 @@ function renderBookmark(arr) {
 
 /* =============== Main Arr editing =============== */
 function mainArrEditing(arr1, arr2) {
-
+    let t;
     arr1.forEach((item) => {
         let obj = {};
+        Math.trunc(item.runtime / 60) > 0 ? t = Math.trunc(item.runtime / 60) : t = item.runtime;
 
         obj.title = item.title;
         obj.year = item.year;
         obj.categories = item.categories;
         obj.id = item.imdbId;
         obj.lang = item.language;
-        obj.time = `${Math.trunc(item.runtime/60)}h:${item.runtime%60}m`;
+        obj.time = `${t}h:${item.runtime%60}m`;
         obj.link = `https://youtu.be/${item.youtubeId}`;
         obj.info = item.summary;
         obj.minImg = item.smallThumbnail.trim();
