1be0916b9280df509003be53882409eaa165dc32
MirkhakimAzizov <azizov.mirkhakimjon@gmail.com>
2023-03-26 11:15:20 +0500

    Movie

--- script.js
+++ script.js
@@ -187,3 +187,44 @@ elList.addEventListener("click", (evt) => {
     }
 
 });
+
+
+elBookmark.addEventListener("click", (evt) => {
+
+    evt.preventDefault();
+
+    elModalBody.innerHTML = "";
+
+    if (evt.target.matches(".btn__more")) {
+        let id = evt.target.dataset.more;
+
+        nevMovies.forEach(item => {
+
+            if (item.id === id) {
+
+                elModalBody.innerHTML = `
+                
+                <img src="${item.minImg}" class="card-img-top mt-5 d-blok" alt="${item.title}">
+                <div class="card-body w-100">
+                <h5 class="card-title my-2">${item.title}</h5>
+                <div class="d-flex gap-3">
+                    <span class="text-warning d-flex align-items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        Rating: ${item.rating}</span>
+                    <span class="text-primary  text-decoration-underline">${item.time}</span>
+                </div>
+                <p class="card-text"><strong>Category: </strong>${item.categories}</p>
+                <p class="card-text"><strong>Summary: </strong>${item.info}</p>
+                <a href="${item.link}" class="card-link btn btn-danger w-100 mx-auto mb-2">YouTube</a>
+
+                `
+
+            }
+
+        });
+
+    }
+
+});
