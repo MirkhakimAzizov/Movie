886e23696ecc7807bea26194b561d34fd94a8ebc
MirkhakimAzizov <azizov.mirkhakimjon@gmail.com>
2023-03-26 06:48:44 +0500

    movie site

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+/* =============== Document elements =============== */
 let elList = document.querySelector(".hero__list");
 let elForm = document.querySelector(".form");
 let elSearchMain = document.querySelector(".header__search");
@@ -10,121 +12,24 @@ let searchBtn = document.querySelector(".form__search");
 let goBack = document.querySelector(".form__all");
 let elBookmark = document.querySelector(".bookmark__list");
 let elBookmarkBtn = document.querySelector(".btn__bookmark");
-// let elSearch = document.querySelector(".hero__search");
-// let elSelectTitle = document.querySelector(".hero__select_title");
-// let elSearchCategory = document.querySelector(".hero__search_category");
-// let elSelectLang = document.querySelector(".hero__select_lang");
-// let elSelectYear = document.querySelector(".hero__select_year");
 
 movies.splice(20);
 
+
+/* =============== New Arr =============== */
 let nevMovies = [];
-
-movies.forEach((item) => {
-    let obj = {};
-
-    obj.title = item.title;
-    obj.year = item.year;
-    obj.categories = item.categories;
-    obj.id = item.imdbId;
-    obj.lang = item.language;
-    obj.time = `${Math.trunc(item.runtime/60)}h:${item.runtime%60}m`;
-    obj.link = `https://youtu.be/${item.youtubeId}`;
-    obj.info = item.summary;
-    obj.minImg = item.smallThumbnail.trim();
-    obj.maxImg = item.bigThumbnail;
-    obj.rating = item.imdbRating;
-
-    nevMovies.push(obj);
-});
-
-renderUi(nevMovies);
-// {/* <img src="${item.minImg}"  class="card-img-top" alt="${item.title}"> */}
-
-
-function renderUi(arr) {
-    arr.forEach(item => {
-
-        let elItem = document.createElement("li");
-        elItem.setAttribute("class", "card");
-        let a = "     ";
-
-        elItem.innerHTML = `
-            <div class="card-body">
-                <h5 class="card-title">${item.title}</h5>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Language: ${item.lang}</li>
-                <li class="list-group-item mr-4"> Rating: ${item.rating}</li>
-                <li class="list-group-item">${item.categories}</li>
-                <li class="list-group-item">
-                    <a href="${item.link}" class="card-link btn btn-danger">YouTube</a>
-                    <a href="${item.link}" class="card-link btn btn-info btn__add" data-id="${item.id}">ADD
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
-                        <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
-                        <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
-                        </svg>
-                    </a>
-                </li>
-            </ul>
-            
-
-        `
-            // srcset="${item.minImg} 1x, ${item.maxImg} 2x"
-        elList.append(elItem);
-
-    });
-}
-
 let arrCategory = [];
-// let arrTitle = [];
-// let arrYear = [];
-// let arrLang = [];
+let bookmarkArr = [];
 
-// nevMovies.forEach(item => {
-//     if (!arrTitle.includes(item.title)) {
-//         arrTitle.push(item.title);
-//     }
-// });
-// nevMovies.forEach(item => {
-//     if (!arrYear.includes(item.year)) {
-//         arrYear.push(item.year);
-//     }
-// });
-// nevMovies.forEach(item => {
-//     if (!arrLang.includes(item.lang)) {
-//         arrLang.push(item.lang);
-//     }
-// });
-nevMovies.forEach(item => {
-    let arr = [];
-    arr.push(item.categories);
-    arr.forEach(el => {
-        el.forEach(i => {
-            if (!arrCategory.includes(i)) {
-                arrCategory.push(i);
-            }
-        })
-    });
-});
 
-// selected(arrTitle.sort(), elSelectTitle);
-// selected(arrLang.sort(), elSelectLang);
-// selected(arrYear.sort((a, b) => a - b), elSelectYear);
+/* =============== Function =============== */
+mainArrEditing(movies, nevMovies);
+renderUi(nevMovies);
+arrSelectFun(nevMovies, arrCategory);
 selected(arrCategory.sort(), elSelectCategory);
 
-function selected(arr, elm) {
 
-    arr.forEach(item => {
-
-        let elOption = document.createElement("option");
-        elOption.textContent = item;
-        elm.append(elOption);
-
-    })
-
-}
-
+/* =============== Main search btn =============== */
 elSearchMain.addEventListener("keyup", (evt) => {
     evt.preventDefault();
     let value = evt.target.value.toUpperCase();
@@ -143,32 +48,7 @@ elSearchMain.addEventListener("keyup", (evt) => {
 });
 
 
-
-// elSelectTitle.addEventListener("change", (evt) => {
-//     evt.preventDefault();
-//     let value = evt.target.value;
-
-//     let arrSelect = [];
-
-//     function selecFun(arr) {
-//         arr.forEach(item => {
-//             if (value == item.title) {
-//                 arrSelect.push(item);
-//             }
-//         })
-//     }
-
-//     if (value == "all") {
-//         elList.innerHTML = '';
-//         renderUi(nevMovies);
-//     } else {
-//         selecFun(nevMovies);
-//         elList.innerHTML = '';
-//         renderUi(arrSelect);
-//     }
-
-// })
-
+/* =============== Search btn =============== */
 searchBtn.addEventListener("click", (evt) => {
 
     evt.preventDefault();
@@ -181,7 +61,7 @@ searchBtn.addEventListener("click", (evt) => {
 
     newMovie = nevMovies.filter((item) => {
 
-        return item.title.includes(valueTitle) && item.rating >= valueRating && item.categories.includes(valueCategory);
+        return item.title.includes(valueTitle) || item.rating >= valueRating || item.categories.includes(valueCategory);
 
     });
 
@@ -195,6 +75,8 @@ searchBtn.addEventListener("click", (evt) => {
 
 });
 
+
+/* =============== Main UI btn =============== */
 goBack.addEventListener("click", (evt) => {
 
     evt.preventDefault();
@@ -204,8 +86,8 @@ goBack.addEventListener("click", (evt) => {
 
 });
 
-let bookmarkArr = [];
 
+/* =============== Bookmark add btn =============== */
 elList.addEventListener("click", (evt) => {
 
     evt.preventDefault();
@@ -217,35 +99,44 @@ elList.addEventListener("click", (evt) => {
             if (item.id == id) {
                 if (!bookmarkArr.includes(item)) {
                     bookmarkArr.push(item);
-                } else {
-                    let i = bookmarkArr.indexOf(item);
-                    bookmarkArr.splice(i, i + 1);
                 }
             }
 
         });
 
     }
-    // console.log(bookmarkArr);
+    elBookmark.innerHTML = 'Bookmark:';
     renderBookmark(bookmarkArr);
 
 });
 
-function minMax(that, value) {
-    let min = parseInt(that.getAttribute("min"));
-    let max = parseInt(that.getAttribute("max"));
-    let val = parseInt(value);
 
-    if (val < min || isNaN(val)) {
-        return min;
-    } else if (val > max) {
-        return max;
-    } else {
-        return val;
+/* =============== Bookmark remote btn =============== */
+elBookmark.addEventListener("click", (evt) => {
+
+    evt.preventDefault();
+    if (evt.target.matches(".btn__add")) {
+        let id = evt.target.dataset.id;
+
+        nevMovies.forEach((item) => {
+
+            if (item.id == id) {
+
+                let i = bookmarkArr.indexOf(item);
+                bookmarkArr.splice(i, i + 1);
+
+            }
+
+        });
+
     }
+    elBookmark.innerHTML = `Bookmark:`;
+    renderBookmark(bookmarkArr);
 
-}
+});
 
+
+/* =============== Dropdown bookmark btn =============== */
 elBookmarkBtn.addEventListener("click", (evt) => {
 
     evt.preventDefault();
@@ -253,37 +144,3 @@ elBookmarkBtn.addEventListener("click", (evt) => {
     elBookmark.classList.toggle("display");
 
 });
-
-function renderBookmark(arr) {
-    arr.forEach(item => {
-
-        let elItem = document.createElement("li");
-        elItem.setAttribute("class", "card");
-        let a = "     ";
-
-        elItem.innerHTML = `
-            <div class="card-body">
-                <h5 class="card-title">${item.title}</h5>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Language: ${item.lang}</li>
-                <li class="list-group-item mr-4"> Rating: ${item.rating}</li>
-                <li class="list-group-item">${item.categories}</li>
-                <li class="list-group-item">
-                    <a href="${item.link}" class="card-link btn btn-danger">YouTube</a>
-                    <a href="${item.link}" class="card-link btn btn-info btn__add" data-id="${item.id}">ADD
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
-                        <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
-                        <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
-                        </svg>
-                    </a>
-                </li>
-            </ul>
-            
-
-        `
-            // srcset="${item.minImg} 1x, ${item.maxImg} 2x"
-        elBookmark.append(elItem);
-
-    });
-}
---
+++ utils.js
@@ -0,0 +1,142 @@
+"use strict";
+
+
+/* =============== Render UI function =============== */
+function renderUi(arr) {
+    arr.forEach(item => {
+
+        let elItem = document.createElement("li");
+        elItem.setAttribute("class", "card");
+        // <img src="${item.minImg}" srcset="${item.minImg} 1x, ${item.maxImg} 2x" class="card-img-top" alt="${item.title}">
+        elItem.innerHTML = `
+            <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Language: ${item.lang}</li>
+                <li class="list-group-item mr-4"> Rating: ${item.rating}</li>
+                <li class="list-group-item">${item.categories}</li>
+                <li class="list-group-item">
+                    <a class="card-link btn btn-success w-100 mx-auto mb-2 btn__more" data-more="${item.id}" >More</a>
+                    <a href="${item.link}" class="card-link btn btn-danger w-100 mx-auto mb-2">YouTube</a>
+                    <a class="card-link btn btn-info btn__add w-100 mx-auto" data-id="${item.id}">ADD
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
+                        <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
+                        <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+            
+
+        `
+        elList.append(elItem);
+
+    });
+}
+
+
+/* =============== Render Bookmark function =============== */
+function renderBookmark(arr) {
+    arr.forEach(item => {
+
+        let elItem = document.createElement("li");
+        elItem.setAttribute("class", "card");
+
+        elItem.innerHTML = `
+            <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Language: ${item.lang}</li>
+                <li class="list-group-item mr-4"> Rating: ${item.rating}</li>
+                <li class="list-group-item">${item.categories}</li>
+                <li class="list-group-item">
+                    <a class="card-link btn btn-success w-100 mx-auto mb-2" data-more="${item.id}" >More</a>
+                    <a href="${item.link}" class="card-link btn btn-danger w-100 mx-auto mb-2">YouTube</a>
+                    <a class="card-link btn btn-warning btn__add w-100 mx-auto" data-id="${item.id}">REMOVE</a>
+                </li>
+            </ul>
+            
+
+        `
+            // srcset="${item.minImg} 1x, ${item.maxImg} 2x"
+        elBookmark.append(elItem);
+
+    });
+}
+
+
+/* =============== Main Arr editing =============== */
+function mainArrEditing(arr1, arr2) {
+
+    arr1.forEach((item) => {
+        let obj = {};
+
+        obj.title = item.title;
+        obj.year = item.year;
+        obj.categories = item.categories;
+        obj.id = item.imdbId;
+        obj.lang = item.language;
+        obj.time = `${Math.trunc(item.runtime/60)}h:${item.runtime%60}m`;
+        obj.link = `https://youtu.be/${item.youtubeId}`;
+        obj.info = item.summary;
+        obj.minImg = item.smallThumbnail.trim();
+        obj.maxImg = item.bigThumbnail;
+        obj.rating = item.imdbRating;
+
+        arr2.push(obj);
+    });
+
+}
+
+
+/* =============== Arr editing category =============== */
+function arrSelectFun(arr1, arr2) {
+
+    arr1.forEach(item => {
+        let arr = [];
+        arr.push(item.categories);
+        arr.forEach(el => {
+            el.forEach(i => {
+                if (!arr2.includes(i)) {
+                    arr2.push(i);
+                }
+            })
+        });
+    });
+
+}
+
+
+/* =============== Selected function =============== */
+function selected(arr, elm) {
+
+    arr.forEach(item => {
+
+        let elOption = document.createElement("option");
+        elOption.textContent = item;
+        elm.append(elOption);
+
+    })
+
+}
+
+
+/* =============== MinMax function =============== */
+function minMax(that, value) {
+    let min = parseInt(that.getAttribute("min"));
+    let max = parseInt(that.getAttribute("max"));
+    let val = parseInt(value);
+
+    if (val) {
+        if (val < min || isNaN(val)) {
+            return min;
+        } else if (val > max) {
+            return max;
+        } else {
+            return val;
+        }
+    }
+
+}
