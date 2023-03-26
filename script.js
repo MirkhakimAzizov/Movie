eb339bc2e495788711e3d7a12cae41c3fdbd280e
MirkhakimAzizov <azizov.mirkhakimjon@gmail.com>
2023-03-26 09:49:17 +0500

    movies

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ let goBack = document.querySelector(".form__all");
 let elBookmark = document.querySelector(".bookmark__list");
 let elBookmarkBtn = document.querySelector(".btn__bookmark");
 let elModalBody = document.querySelector(".modal-body");
-console.log(elModalBody);
 
-movies.splice(20);
+
+movies.splice(50);
 
 
 /* =============== New Arr =============== */
@@ -35,7 +35,6 @@ selected(arrCategory.sort(), elSelectCategory);
 elSearchMain.addEventListener("keyup", (evt) => {
     evt.preventDefault();
     let value = evt.target.value.toUpperCase();
-    console.log(value);
 
     if (value != "" || value != " ") {
         let newArr = nevMovies.filter(item => {
@@ -152,6 +151,9 @@ elBookmarkBtn.addEventListener("click", (evt) => {
 elList.addEventListener("click", (evt) => {
 
     evt.preventDefault();
+
+    elModalBody.innerHTML = "";
+
     if (evt.target.matches(".btn__more")) {
         let id = evt.target.dataset.more;
 
@@ -164,7 +166,7 @@ elList.addEventListener("click", (evt) => {
                 <img src="${item.minImg}" class="card-img-top mt-5 d-blok" alt="${item.title}">
                 <div class="card-body w-100">
                 <h5 class="card-title my-2">${item.title}</h5>
-                <div class="d-flex gap-4">
+                <div class="d-flex gap-3">
                     <span class="text-warning d-flex align-items-center gap-1">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
--- utils.js
+++ utils.js
@@ -14,7 +14,7 @@ function renderUi(arr) {
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">
-                    <div class="d-flex gap-4">
+                    <div class="d-flex justify-content-between">
                         <span class="text-warning d-flex align-items-center gap-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
@@ -26,7 +26,7 @@ function renderUi(arr) {
                     <div></div>
                 </li>
                 <li class="list-group-item">
-                    <a class="card-link btn btn-success w-100 mx-auto mb-2 btn__more" data-more="${item.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">More</a>
+                    <a class="card-link btn btn-success w-100 mx-auto mb-2 btn__more" data-more="${item.id}" data-bs-toggle="modal" data-bs-target="#Modal">More</a>
                     <a href="${item.link}" class="card-link btn btn-danger w-100 mx-auto mb-2">YouTube</a>
                     <a class="card-link btn btn-info btn__add w-100 mx-auto" data-id="${item.id}">ADD
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
